4241ff98b64ffa62a76c3dd42d20f17b1b264946
2025csbindusb-rgb <2025cs_bindus_b@nie.ac.in>
2026-09-22 15:20:42 +0530

    pyantic

--- Day/Day2/FastAPIPractice/main.py
+++ Day/Day2/FastAPIPractice/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import basemodel
 app=FastAPI()
 @app.get("/")#request to server--get,/-default route
 def home():
@@ -22,3 +23,13 @@ def get_result(rollNo):
 @app.get("/candidate/{rollNo}")
 def get_candidate(rollNo:int):
     return {"Result":"Distinction","rollNo":rollNo}
+
+pyantic
+class Item(Basemodel):
+    name:str
+    price:float
+    in_stock: bool= True
+
+@app.post("/items")
+def creat_item(item:Item):
+    return {"received":item,"total_price":item.price*1.18}
